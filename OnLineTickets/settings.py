6bee3a1d86e7aae9c7a070d65c016c7f8ac8c63b
MoisesFigueroa <moisesfigueroadeveloper@gmail.com>
2023-06-13 03:35:40 -0400

    Importacion de Boostrap5

--- OnLineTickets/settings.py
+++ OnLineTickets/settings.py
@@ -41,7 +41,13 @@ INSTALLED_APPS = [
     'app.apps.AppConfig',
     'colorfield',
     'django.contrib.humanize',
+    'crispy_forms',
+    'bootstrap5',
+    'crispy_bootstrap5'
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK ="bootstrap5"
 
 X_FRAME_OPTIONS = "SAMEORIGIN"
 
