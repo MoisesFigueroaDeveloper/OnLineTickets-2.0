a31db22b962fa5740e011bfe7e7bf60416e32247
MoisesFigueroa <moisesfigueroadeveloper@gmail.com>
2023-06-14 04:35:47 -0400

    Paginator pendiente modificar los entity

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Eventos
 from .forms import ContactanosForm, EventosForm
+from django.contrib import messages
+from django.core.paginator import Paginator
+from django.http import Http404 
 # Create your views here.
 
 #@login_required
@@ -56,6 +59,13 @@ def agregar_eventos(request):
 
 def listar_eventos(request):
     eventos = Eventos.objects.all()
+    page = request.GET.get('page', 1)
+    
+    try: 
+        paginator = Paginator(eventos, 5)
+        eventos = paginator.page(page)
+    except:
+        raise Http404
     
     data = {
         'eventos': eventos
@@ -74,6 +84,7 @@ def modificar_eventos(request, id):
         formulario = EventosForm(data=request.POST, instance=eventos, files=request.FILES)
         if formulario.is_valid():
             formulario.save()
+            messages.success(request, "Modificacion exitosa")
             return redirect(to="listar_eventos")
         data["form"] = formulario
         
